5fe0f592c70441d7bf51230e9cb4073c4c766cad
anbundang <anbundang@aol.com>
2020-05-17 14:23:40 -0700

    first commit

---
+++ OopsDemo.py
@@ -0,0 +1,34 @@
+# classes are user defined blueprint or prototype
+# sum, multiplication, addition, constant
+# methods, class variables, instance variables, constructor etc
+# self keyword is mandatory for calling variable names into method
+# instance and class variables have whole different purpose
+# constructor name should be __init__(self)
+# new keyword is not required when you create object
+
+
+class Calculator:
+    num = 100  # class variables
+    # default constructor
+
+    def __init__(self, a, b):
+        self.firstNumber = a
+        self.secondNumber = b  # instance variables
+        print("I am called automatically when object is created")
+
+    def getData(self):
+        print("Now executing as a method in class")
+
+    def summation(self):
+        return self.firstNumber + self.secondNumber + Calculator.num
+
+
+obj = Calculator(2, 3)  # syntax to create objects in python
+obj.getData()
+obj.num
+print(obj.summation())
+
+obj1 = Calculator(4, 5)  # syntax to create objects in python
+obj1.getData()
+obj1.num
+print(obj1.summation())
---
+++ loops.py
@@ -0,0 +1,35 @@
+# if statements
+greeting1 = "good morning"
+greeting2 = "good MORNINg"
+greeting2 = greeting2.lower()
+if greeting1 == greeting2:
+    print("Condition matches")
+    print("second line")
+else:
+    print("condition does not match")
+print("if else condition code is complete")
+print("************************************************")
+
+# for loop
+obj = [2, 3, 16, 7, 56]
+for element in obj:
+    if element%2 == 0:
+        print(element)
+print("************************************************")
+
+# sum of First Natural numbers 1+2+3+4+5
+# range(i, j) -> i to j-1
+count = 0
+for value in range(1, 6):
+    count = count + value
+print(count)
+print("************************************************")
+
+# this means start from 1 and go up to, but not including, 10 and increment by 3
+for k in range(1, 10, 3):
+    print(k)
+print("************************************************")
+
+# this means to start from 0 and go up to, but not including, 10
+for m in range(10):
+    print(m)
